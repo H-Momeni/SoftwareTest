b88ff96db1dda856057541cce9f040801fe2237d
H-Moemni <hannan.mgh@gmail.com>
2025-02-01 13:34:25 +0330

    delete

--- zahra/test2.py
+++
@@ -1,19 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Firefox()
-driver.get("https://shop.polymer-project.org/")
-time.sleep(3)
-
-# دریافت عنوان واقعی صفحه
-actual_title = driver.title
-print(f"🔍 عنوان صفحه دریافت شده: '{actual_title}'")
-
-expected_title = "Home - SHOP"  # مقدار صحیح را اینجا قرار بده
-
-if actual_title == expected_title:
-    print("✅ عنوان صفحه صحیح است.")
-else:
-    print(f"❌ خطا: عنوان صفحه '{actual_title}' است، درحالی که باید '{expected_title}' باشد.")
-
-driver.quit()
